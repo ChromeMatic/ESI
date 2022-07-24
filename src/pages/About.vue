d9bca7b199b7b44b386d66939837f69708dacf39
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-07-24 17:42:05 -0500

    change to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,47 +7,63 @@ import {UseThemeStore} from "../store/index"
  const Team = [
   {
     name:'lorenzo gunter',
-    img:'/man.png'
+    img:'/man.png',
+    role:'Chief Executive Officer (CEO) / Co-founder / General Manager'
   },
   {
     name:'Daltonett Griffiths',
-    img:'/woman.png'
+    img:'/woman.png',
+    role:'Company Secretary / Software Security Expert'
   },
   {
     name:'Dwayne Pusey',
-    img:'/man2.png'
+    img:'/man2.png',
+    role:'Executive Director / Systems Analysis / Network Expert'
   },
   {
     name:'Alrick Franklin',
-    img:'/gamer.png'
+    img:'/gamer.png',
+    role:'Executive Director / Frontend Developer / Mobile Developer'
   },
   {
     name:'Armani Brown',
-    img:'/hacker.png'
+    img:'/hacker.png',
+    role:'Chief Operating Officer / Co-founder / DevOps Expert'
   },
   {
     name:'Michael Whyne Jr',
-    img:'/man3.png'
+    img:'/man3.png',
+    role:'Executive Director / UI Designer / Frontend Developer'
   },
   {
     name:'Christopher Scott',
-    img:'/programmer2.png'
+    img:'/programmer2.png',
+    role:'Executive Director / Frontend Developer'
   },
   {
     name:'Edwin Miller',
-    img:'/man2.png'
+    img:'/man2.png',
+    role:'Chief Information Officer CIO / Full-Stack Developer'
   },
   {
     name:'Leo Mitchell',
-    img:'/avatar.png'
+    img:'/avatar.png',
+    role:'Chief Marketing Officer CMO'
   },
   {
     name:"Ackeem Millen",
-    img:'/man4.png'
+    img:'/man4.png',
+    role:'Consultant / Web Developer'
   },
-   {
+  {
+    name:"Matthew Atkinson",
+    img:'/man3.png',
+    role:'Chief Financial Officer'
+  },
+  {
     name:"Jontae",
-    img:'/man.png'
+    img:'/man.png',
+    role:'Video Editor'
   }
  ]
 </script>
@@ -68,14 +84,18 @@ import {UseThemeStore} from "../store/index"
       <div
        v-for="item in Team" :key="item.name"
        :class="darkMode.isDark ? 'bg-innerDark transition-all':'transition-all bg-purple-900' "
-       class="w-full py-2 rounded-md flex flex-col justify-center items-center text-white
-       transition-all hover:scale-y-110 hover:scale-x-110"
+       class="w-full py-2 px-6 rounded-md flex flex-col justify-center items-center text-white
+       transition-all hover:scale-y-110 hover:scale-x-110 space-y-4"
       >
         <img :src="item.img" class="h-32 w-32" alt="">
 
-        <h1>
+        <h1 class="text-xl uppercase">
           {{item.name}}
         </h1>
+        
+        <p class="font-semibold uppercase text-base text-center">
+          {{item.role}}
+        </p>
       </div>
 
     </div>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -27,21 +27,22 @@
          class="lg:text-6xl text-3xl text-white uppercase font-TW text-center">
             welcome to entech software innovations
          </h1>
-         <p class="text-white capitalize font-TW text-center text-2xl">
-           need a website for you or your bushiness? no worry
+         <p class="text-white font-TW text-center text-2xl">
+           Need a website for personal or bushiness use? No worries
            leave the heavy lifting to us.
          </p>
       </div>
       
     </div>
     
-    <div class="flex flex-col justify-center items-center py-4 lg:px-8 px-4">
+    <div class="flex flex-col justify-center items-center py-4 lg:px-8 px-4"> 
 
       <h1 class="lg:text-5xl text-3xl uppercase text-white font-TW lg:py-16 py-8">
        What we do
       </h1>
 
-      <div  class="grid lg:grid-cols-3 grid-cols-1 lg:gap-4 gap-2 w-full">
+      <div  class="grid lg:grid-cols-3 grid-cols-1 lg:gap-2 gap-8 w-full">
+
 
          <div
           v-motion="slideLeft"
@@ -51,10 +52,9 @@
          >
           <img v-motion="motion1" src="../assets/undraw_code_inspection_bdl7.svg" class="w-full h-60" alt="">
           <div class="w-full py-4 px-8 flex flex-col space-y-4 justify-center items-center font-TW">
-            <h1 class="uppercase text-2xl">What we do</h1>
-            <p class="uppercase text-center">
-              here at ESI we offer a wide range software solutions for your bushiness,
-              form website development to app development.
+            <p class="uppercase text-center leading-loose">
+              Here at ESI we offer a wide range of software solutions for your business. From website development to 
+              app development and everything in between.
             </p>
             <button
               @click="toServices"
@@ -74,10 +74,9 @@
          >
           <img v-motion="motion2" src="../assets/undraw_dev_productivity_re_fylf.svg" class="w-full h-60" alt="">
           <div class="w-full py-4 px-8 flex flex-col space-y-4 justify-center items-center">
-            <h1 class="uppercase text-2xl">web development</h1>
-            <p class="uppercase text-center">
-              here at ESI we can develop a website for or your bushiness. Need an online store to
-              sell to your customers, don't worry we can build that for you.
+            <p class="uppercase text-center leading-loose">
+             Here at ESI we develop websites for Businesses as well as individuals. Looking to branch into 
+             E-Commerce? We can help you create your space and build an image.
             </p>
              <button
              @click="toServices" 
@@ -102,10 +101,9 @@
            v-motion="motion3"
           >
           <div class="w-full py-4 px-8 flex flex-col space-y-4 justify-center items-center font-TW">
-            <h1 class="uppercase text-2xl">Management</h1>
-            <p class="uppercase text-center">
-              There is no need for you to stress yourself over the day to day management of your websites
-              leave that to us. We will ensure that your website is always running in tip-top shape.
+            <p class="uppercase text-center leading-loose">
+              There is no need to stress. That’s it. We will ensure that your website is always running 
+              in tip-top shape.
             </p>
              <button 
               @click="toServices"
@@ -134,10 +132,10 @@
           v-motion="motion1"
         >
           <p 
-           class="font-TW  text-lg lg:tracking-wide tracking-normal leading-loose subpixel-antialiased text-justify"
+           class="font-TW  text-lg lg:tracking-normal text-center leading-loose"
           >
             Hello, and welcome to ESI (Entech Software Innovations). We are a startup tech 
-            company located in the beautiful island of Jamaica. We are recent graduates of Northern 
+            company located on the beautiful island of Jamaica. We are recent graduates of Northern 
             Caribbean University. Our fields of study range from Computer Science to Computer information 
             Systems. We are a diverse team with unique skills ranging form web development to app development
             and Software Security, along with UI designers. We are a young and talented team with real world 
